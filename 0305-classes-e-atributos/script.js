d17ace0b39da51d12274deec23f025f2b9f80cd8
Iago Balbino <iago_nobre@hotmail.com>
2020-10-26 15:42:55 -0300

    Aula 0305 - Classes e Atributos

---
+++ 0305-classes-e-atributos/script.js
@@ -0,0 +1,12 @@
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach.add('ativo');
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+itensMenu.
+
+// Verifique se as imagens possuem o atributo alt
+
+// Modifique o href do link externo no menu
+
